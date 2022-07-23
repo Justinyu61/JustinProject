6c2abb8a359349a2af8b681d6435313589a09842
Justinyu61 <justinyu61@gmail.com>
2022-07-23 14:01:55 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,19 @@
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+  {{ name }}
+  <button type="button" class="btn btn-primary">Primary</button>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data () {
+    return {
+      name: process.env.VUE_APP_NAME
     }
   }
 }
+</script>
+
+<style lang="scss">
+@import "~bootstrap";
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Card></Card>
   </div>
 </template>
+<script>
+import Card from '@/components/Card.vue'
+export default {
+  components: {
+    Card
+  }
+}
+</script>
