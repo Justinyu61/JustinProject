bb032f02246cbb47db34cc3aa734678f3618cd56
Justinyu61 <justinyu61@gmail.com>
2022-07-26 23:11:05 +0800

    1.新增utils/request將API get方法寫入 2.將axios.baseURL封裝出去

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,23 @@
 import axios from 'axios'
 
+const instance = axios.create({
+  baseURL: process.env.VUE_APP_API,
+  timeout: 10000
+})
+
+export const get = (url, params = {}) => {
+  return new Promise((resolve, reject) => {
+    instance.post(url, { params }).then((res) => {
+      resolve(res)
+    }, err => {
+      reject(err)
+    })
+  })
+}
+
 export const post = (url, data = {}) => {
   return new Promise((resolve, reject) => {
-    axios.post(url, data, {
-      baseURL: process.env.VUE_APP_API,
-      timeout: 5000
-    }).then((res) => {
+    instance.post(url, data).then((res) => {
       resolve(res)
     }, err => {
       reject(err)
