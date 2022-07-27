5a190a8a923fd0392e5678b7617bb066f4060b68
Justinyu61 <justinyu61@gmail.com>
2022-07-28 00:34:59 +0800

    1.新增Loading動畫效果

--- src/main.js
+++ src/main.js
@@ -3,5 +3,9 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
+// Import component
+import Loading from 'vue3-loading-overlay'
+// Import stylesheet
+import 'vue3-loading-overlay/dist/vue3-loading-overlay.css'
 
-createApp(App).use(VueAxios, axios).use(router).mount('#app')
+createApp(App).component('Loading', Loading).use(VueAxios, axios).use(router).mount('#app')
